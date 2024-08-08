e50d87661d59def6d232ef81c7000050b5156396
r0drigo45 <oliveira988r@gmail.com>
2024-08-08 20:10:03 -0300

    caminho dinamico

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
+def ler_nome_arq():
+    print('insira o caminho do arquivo')
+    # lendo caminho para o arquivo do stdin
+    caminho = input('>> ')
+    return str(caminho)
+
 def main():
-    arquivo = open('teste.txt','r')
+    caminho = ler_nome_arq()
+    arquivo = open(caminho,'r')
     texto = arquivo.read()
+    # split sem argumentos usa whitespace como separador
+    # e quebra todas as ocorrências em uma lista de strings
     split = texto.split()
-    print(len(texto))
+    print(len(split))
 
 
 main()
